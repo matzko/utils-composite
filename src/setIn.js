824cc5b850a8f5b98bb1b997327cdba8d032b0fc
Mauro Titimoli <mgtitimoli@gmail.com>
2017-09-20 21:03:03 -0300

    docs(src): Improve documentation

--- src/setIn.js
+++ src/setIn.js
@@ -8,7 +8,7 @@ import type {Composite, Path} from "./types";
  * Returns a new composite with the result of having updated the property
  * located at the given path with the specified value.
  * 
- * (This does the same as updateIn with updater: () => value)
+ * (This does the same as calling **updateIn** with updater: `() => value`)
  */
 const setIn = (path: Path, value: mixed, composite: Composite) =>
   updateIn(path, () => value, composite);
