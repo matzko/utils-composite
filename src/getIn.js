ce0f41d2b157470c085b20f60a27ef28a34400ae
Mauro Titimoli <mgtitimoli@gmail.com>
2017-09-21 21:43:01 -0300

    fix(src): Add logic to handle empty path usecase

--- src/getIn.js
+++ src/getIn.js
@@ -1,18 +1,24 @@
 // @flow
 
+import {isLastIndex} from "@jumpn/utils-array";
+
+import get from "./get";
 import hasKey from "./hasKey";
 
 import type {Composite, Path} from "./types";
 
+const getInRecur = (index, path, composite) => {
+  if (isLastIndex(path, index)) return get(path[index], composite);
+
+  return hasKey(path[index], composite)
+    ? getInRecur(index + 1, path, get(path[index], composite))
+    : undefined;
+};
+
 /**
  * Returns value located at the given path or undefined otherwise.
  */
-const getIn = (path: Path, composite: Composite) => {
-  const [key, ...rest] = path;
-
-  return hasKey(key, composite)
-    ? getIn(rest, composite[(key: any)])
-    : undefined;
-};
+const getIn = (path: Path, composite: Composite) =>
+  path.length === 0 ? undefined : getInRecur(0, path, composite);
 
 export default getIn;
