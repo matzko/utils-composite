975473eca4f5707f096693906198ab1a721a1b19
Mauro Titimoli <mgtitimoli@gmail.com>
2017-09-21 21:46:07 -0300

    fix(src): Update index using build:index script

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 // @flow
 
-export {default as get} from "./get";
-export {default as getIn} from "./getIn";
-export {default as getKeys} from "./getKeys";
-export {default as hasKey} from "./hasKey";
-export {default as hasIn} from "./hasIn";
-export {default as haveSameProps} from "./haveSameProps";
-export {default as remove} from "./remove";
-export {default as removeIn} from "./removeIn";
-export {default as set} from "./set";
-export {default as setIn} from "./setIn";
-export {default as shallowCopy} from "./shallowCopy";
-export {default as shallowEqual} from "./shallowEqual";
-export {default as updateIn} from "./updateIn";
+// @create-index
+
+export {default as get} from "./get.js";
+export {default as getIn} from "./getIn.js";
+export {default as getKeys} from "./getKeys.js";
+export {default as hasIn} from "./hasIn.js";
+export {default as hasKey} from "./hasKey.js";
+export {default as haveSameProps} from "./haveSameProps.js";
+export {default as remove} from "./remove.js";
+export {default as removeIn} from "./removeIn.js";
+export {default as set} from "./set.js";
+export {default as setIn} from "./setIn.js";
+export {default as shallowCopy} from "./shallowCopy.js";
+export {default as shallowEqual} from "./shallowEqual.js";
+export {default as types} from "./types.js";
+export {default as updateIn} from "./updateIn.js";
