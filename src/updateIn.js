dec4e6fd5e81f9edb6b63e9f8b8eed1dd925aa20
Mauro Titimoli <mauro@jumpn.com>
2017-10-02 20:35:19 -0300

    fix(src): Fix invalid result when path has only one key

--- src/updateIn.js
+++ src/updateIn.js
@@ -8,7 +8,7 @@ import hasKey from "./hasKey";
 import remove from "./remove";
 import shallowCopy from "./shallowCopy";
 
-import type {Composite, Key, Path} from "./types";
+import type {Composite, Path} from "./types";
 
 const createReduceContext = composite => {
   const origin = shallowCopy(composite);
@@ -28,19 +28,27 @@ const getNext = (path, updater, index, current) =>
     ? updater(get(path[index], current))
     : copyOrCreate(path[index], path[index + 1], current);
 
-const set = (key: Key, value: mixed, composite: Composite) => {
+const set = (key, value, composite) => {
   // eslint-disable-next-line no-param-reassign
   composite[(key: any)] = value;
 
   return get(key, composite);
 };
 
+const updateSet = (path, index, value, context) => ({
+  ...context,
+  current: set(path[index], value, context.current)
+});
+
 const updateRemove = (path, index, context) => {
   const removed = remove(path[index], context.current);
 
   return index === 0
-    ? removed
-    : set(path[index - 1], removed, (context.previous: any));
+    ? {...context, current: removed, origin: removed}
+    : {
+        ...context,
+        previous: set(path[index - 1], removed, (context.previous: any))
+      };
 };
 
 const removeAction = Symbol("composite.updateIn.removeAction");
@@ -48,23 +56,10 @@ const removeAction = Symbol("composite.updateIn.removeAction");
 const update = (path, index, value, context) =>
   value === removeAction
     ? updateRemove(path, index, context)
-    : set(path[index], value, context.current);
+    : updateSet(path, index, value, context);
 
-const getReducer = (path, updater) => (context, key, index) => ({
-  origin: context.origin,
-  current: update(
-    path,
-    index,
-    getNext(path, updater, index, context.current),
-    context
-  ),
-  previous: context.current
-});
-
-// we are doing this as if path has one key and this one has been removed,
-// then the result would be in current
-const getResult = (path, context) =>
-  path.length === 1 ? context.current : context.origin;
+const getReducer = (path, updater) => (context, key, index) =>
+  update(path, index, getNext(path, updater, index, context.current), context);
 
 /**
  * Returns a new composite with the result of having updated the property value
@@ -80,10 +75,8 @@ const updateIn = (
 ): Composite =>
   path.length === 0
     ? composite
-    : getResult(
-        path,
-        path.reduce(getReducer(path, updater), createReduceContext(composite))
-      );
+    : path.reduce(getReducer(path, updater), createReduceContext(composite))
+        .origin;
 
 // we are doing this way and not returning an Object.assign construction, as
 // that is not well typed (returns any)
